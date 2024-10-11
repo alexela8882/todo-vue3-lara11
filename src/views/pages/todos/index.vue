8d510a292b5ea8bfdaee246f436fc781b97f43ed
alexela8882 <alexela8882@gmail.com>
2024-10-11 09:24:21 +0800

    ux enhancement

--- src/views/pages/todos/index.vue
+++ src/views/pages/todos/index.vue
@@ -24,8 +24,11 @@
 
     <!-- Legends -->
     <div class="flex gap-1 mb-4">
-      <div v-for="(status, sx) in getTodoStatuses" :key="sx">
-        <button class="btn">
+      <div v-for="(status, sx) in todoStatuses" :key="sx">
+        <button
+          @click="status.toggle = !status.toggle"
+          class="btn"
+          :class="`${statusClass('border', status.id)} border-${status.toggle ? '1' : '0'}`">
           {{ status.name }}
           <div class="text-white badge badge-sm p-2" :class="statusClass('bg', status.id)">
             {{ todoStatusCount(status.id) }}
@@ -34,9 +37,13 @@
       </div>
     </div>
 
-    <div>
+    <div v-if="toggledStatuses && toggledStatuses.length <= 0">
+      No toggled todos
+    </div>
+    <div v-else>
       <div
-        v-for="todo in getTodos"
+        v-if="filteredTodos && filteredTodos.length > 0"
+        v-for="todo in filteredTodos"
         :key="todo.id">
         <div
           class="card bg-base-100 shadow-xl mb-4 border border-t-4"
@@ -94,14 +101,6 @@
               placeholder="Description"
               class="textarea textarea-bordered w-full"
             ></textarea>
-
-            <div>
-              <button @click="_deleteTodo(todo.id)" class="btn btn-sm text-white btn-error">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                </svg>
-              </button>
-            </div>
           </div>
 
           <!-- Content -->
@@ -110,6 +109,9 @@
             <p>{{ todo.description }}</p>
           </div>
         </div>
+      </div>
+      <div v-else>
+        No TODOS. Please create one.
       </div>
     </div>
 
@@ -131,7 +133,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { storeToRefs } from 'pinia'
 import axios from '@/axios'
 
@@ -171,6 +173,16 @@ const statusClass = (prefix, statusId) => {
   }
 }
 
+const toggledStatuses = computed(() => {
+  // Get the ids of the statuses where toggle is true
+  return todoStatuses.value.filter(status => status.toggle).map(status => status.id)
+})
+
+const filteredTodos = computed(() => {
+  // Filter todos based on whether their status id is in the toggled statuses
+  return getTodos.value.filter(todo => toggledStatuses.value.includes(todo.todo_status_id))
+})
+
 // Create a new todo
 const _storeTodo = async () => {
   if (!newTodo.value) return
@@ -208,6 +220,12 @@ const _deleteTodo = async (id) => {
 onMounted(async () => {
   await fetchTodos()
   await fetchTodoStatuses()
+
+  // assign
+  todoStatuses.value = getTodoStatuses.value.map(status => ({
+    ...status,
+    toggle: true // toggle all
+  }))
 })
 </script>
 
